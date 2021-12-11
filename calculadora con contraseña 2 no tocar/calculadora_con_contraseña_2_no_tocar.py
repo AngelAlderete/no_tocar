e09ca641d9db1d338ca17a7860a14fcfb29b8409
AngelAlderete <mismarttv321@gmail.com>
2021-12-11 17:19:00 -0400

    Agregar archivos de proyecto.

---
+++ calculadora con contraseña 2 no tocar/calculadora_con_contraseña_2_no_tocar.py
@@ -0,0 +1,8 @@
+
+A1 = int(input("password:\n"))
+if A1 == 4142:
+    print("como llegaste aca...\n Que paso....\nPorque hise esto....\n Este es mi fin? \n Pues no...\n")
+    print("DA IGUAL,MI VIDA IRA PERFECTAMENTE IGUAL\nLo que pasa es que mi puta madre que la odio con toda mi alma si es que existiera(el alma)\nmato a un gatito que tenia.... de la peor manera posible \nlo golpeo probablemente la rompio el cuello y murio yo lo tenia en mis manos\nviendo como se retorcia intentando respirar\n lamentablemente la hija de puta me quito el telefono para poner la foto con la excusa de mierda \nde que me alejaba de dios,ella sabia que era ateo y odio TODAS las relgiones por diversos motivos\nuno de ellos es que ella me intento\nmeter ala fuerza esa puta mierda ultra religiosa extremista,xenofoba,homofobica\nPERO MIRA QUE TAL,TE SALIO MAL HIJA DE PUTA \nA LA QUE LLAMO MAMA POR OBLIGACION\nsolo vivo con ella porque soy menor de edad\nnunca lo olvidare...\nnota 1 Angel Alderete 11/12/2021\n")
+else:
+    print("password incorrecta")
+exit()
